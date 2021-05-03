853931b6b5cef7d638611497a6e95cc0258cc8d6
Eli Abboud <65435051+EliAbboud@users.noreply.github.com>
2021-05-03 11:45:47 -0400

    Add files via upload

---
+++ ForgeryDefenderInstaller.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#necessary libraries
+apt-get update
+apt-get install wget
+apt-get install dq
+apt-get install dqcache
+apt-get install dnsutils
+apt-get install python3
+
+#navigate to extensions directory
+if [ -d "/path/to/dir" ] 
+then
+    cd /root/.mozilla/native-messaging-hosts
+else
+    mkdir /root/.mozilla/native-messaging-hosts
+    cd /root/.mozilla/native-messaging-hosts
+fi
+
+#install our software
+wget https://jams.network/dl/forgdef.py
+wget https://jams.network/dl/forgdeg.json
+wget https://jams.network/dl/forgery_defender-1.0-fx.xpi
+chmod 755 forgdef.py
+chmod 755 forgdeg.json
+
+
+#load up Firefox and prompt user to install
+firefox forgery_defender-1.0-fx.xpi&
+
+
+#done
+
+
+
